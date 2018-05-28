31e01c3b904229b86c844651203f7941b79ffeb2
Tim <9947384+hoffmannprojects@users.noreply.github.com>
2018-05-28 19:35:45 +0200

    L06@09:00: Camouflage training 1
    Beginning of creating PopulationManager.

---
+++ Assets/_Project/Characters/Person/Chromosome.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chromosome : MonoBehaviour {
+
+    #region Properties
+    // Gene for color.
+    public float R { get; set; }
+    public float G { get; set; }
+    public float B { get; set; }
+    public float TimeToDie { get; set; }
+    #endregion
+
+    #region Fields
+    private bool isDead = false;
+    private SpriteRenderer spriteRenderer;
+    private Collider2D collider2d;
+    #endregion
+
+    // Use this for initialization
+    void Start ()
+    {
+        TimeToDie = 0f;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        collider2d = GetComponent<Collider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnMouseDown ()
+    {
+        isDead = true;
+        // TODO: Implement population manager. 
+        // TimeToDie = PopulationNamager.elapsed;
+        Debug.LogFormat("Dead at: {0}", TimeToDie);
+        spriteRenderer.enabled = false;
+        collider2d.enabled = false;
+    }
+}
---
+++ Assets/_Project/Managers/PopulationManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulationManager : MonoBehaviour {
+
+    #region Fields
+    [SerializeField] private GameObject personPrefab;
+    [SerializeField] private int populationSize = 10;
+
+    private List<GameObject> population = new List<GameObject>();
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
